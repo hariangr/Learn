4badb0842d2c7e294c7eeafbfd05652a38095193
harianugrah <code.anugrah@gmail.com>
2018-12-05 21:28:56 +0800

    init

---
+++ lib/screens/AnimationsListScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnimationListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Text('Body'),
+    );
+  }
+}
